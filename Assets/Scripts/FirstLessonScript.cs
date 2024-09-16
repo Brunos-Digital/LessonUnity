f293a45244625a7697e3ba8b2f34b1660eb25db0
Bercan Özcan <bercanozcan@gmail.com>
2024-09-16 11:12:41 +0300

    Update FirstLessonScript.cs

--- Assets/Scripts/FirstLessonScript.cs
+++ Assets/Scripts/FirstLessonScript.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstScript : MonoBehaviour
+namespace LessonsBasic
 {
-    // Start is called before the first frame update
-    void Start()
+    public class FirstScript : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        public int startPoint = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
